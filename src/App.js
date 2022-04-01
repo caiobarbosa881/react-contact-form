16cfce48d27fffc8b8bebf15fff842122ca8bf26
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-01 10:59:57 -0300

    improving the design of the form

--- src/App.js
+++ src/App.js
@@ -1,44 +1,11 @@
 import React from 'react';
 import './App.css';
-import { Formik, Field, Form } from 'formik';
-import schema from './schema';
+import MainForm from './components/MainForm';
 
 function App() {
- function onSubmit(values, actions) {
-    console.log('submit', values);
- }
 
   return (
-    <div className="App">
-      <Formik
-      validationSchema={schema}
-      onSubmit={onSubmit}
-      validationOnMount
-      initialValues={{
-        name: '',
-        email: '',
-      }}
-      render={({ values, errors }) => (
-      <Form>
-          <div>
-            <label>Nome</label>
-            <Field name="name" type="text"/>
-            {errors.name && (
-              <span>{errors.name}</span>
-            )}
-          </div>
-          <div>
-            <label>Email</label>
-            <Field name="email" type="email"/>
-            {errors.email && (
-              <span>{errors.email}</span>
-            )}
-          </div>
-          <button type='submit'>Enviar</button>
-          </Form>
-          )}
-        />
-    </div>
+   <MainForm />
   );
 }
 
---
+++ src/components/MainForm.js
@@ -0,0 +1,68 @@
+import React from 'react';
+import { Formik, Field, Form } from 'formik';
+import schema from '../schema';
+import './MainForm.css';
+
+function MainForm() {
+
+    function onSubmit(values, actions) {
+        console.log('submit', values);
+ }
+
+  return (
+    <div className="form-container">
+    <Formik
+    validationSchema={schema}
+    onSubmit={onSubmit}
+    validationOnMount
+    initialValues={{
+      name: '',
+      email: '',
+    }}
+    render={({ values, errors }) => (
+    <Form>
+        <h1>Contate-nos</h1>
+        <div>
+          <label>Nome</label>
+          <Field className='input' name="name" type="text"/>
+          {errors.name && (
+            <span>{errors.name}</span>
+          )}
+        </div>
+        <div>
+          <label className='labels' >Email</label>
+          <Field className='input' name="email" type="email"/>
+          {errors.email && (
+            <span>{errors.email}</span>
+          )}
+        </div>
+        <div>
+          <label>Celular</label>
+          <Field className='input' name="email" type="email"/>
+          {errors.email && (
+            <span>{errors.email}</span>
+          )}
+        </div>
+        <div>
+          <label>CPF</label>
+          <Field className='input' name="email" type="email"/>
+          {errors.email && (
+            <span>{errors.email}</span>
+          )}
+        </div>
+        <div>
+          <label>Cidade</label>
+          <Field className='input' name="email" type="email"/>
+          {errors.email && (
+            <span>{errors.email}</span>
+          )}
+        </div>
+        <button type='submit'>Enviar</button>
+        </Form>
+        )}
+      />
+  </div>
+  )
+}
+
+export default MainForm
