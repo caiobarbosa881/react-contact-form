2b77070d447ccefc694ad4a652a6b3972f975d95
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-02 13:08:43 -0300

    updating console and error messages

--- src/components/MainForm.js
+++ src/components/MainForm.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Formik, Field, Form } from 'formik';
+import { useState } from 'react';
 import schema from '../schema';
 import './MainForm.css';
 
 function MainForm() {
 
-    function onSubmit(values, actions) {
+    function onSubmit(values) {
         console.log('submit', values);
  }
 
@@ -18,43 +19,46 @@ function MainForm() {
     initialValues={{
       name: '',
       email: '',
+      celular: '',
+      cpf: '',
+      cidade: '',
     }}
-    render={({ values, errors }) => (
+    render={({ errors }) => (
     <Form>
         <h1>Contate-nos</h1>
         <div>
           <label>Nome</label>
           <Field className='input' name="name" type="text"/>
           {errors.name && (
-            <span>{errors.name}</span>
+            <p className='error'>{errors.name}</p>
           )}
         </div>
         <div>
-          <label className='labels' >E-mail</label>
+          <label>E-mail</label>
           <Field className='input' name="email" type="email"/>
           {errors.email && (
-            <span>{errors.email}</span>
+            <p className='error'>{errors.email}</p>
           )}
         </div>
         <div>
           <label>Celular</label>
-          <Field className='input' name="email" type="email"/>
-          {errors.email && (
-            <span>{errors.email}</span>
+          <Field className='input' name="celular" type="text"/>
+          {errors.celular && (
+            <p className='error'>{errors.celular}</p>
           )}
         </div>
         <div>
           <label>CPF</label>
-          <Field className='input' name="email" type="email"/>
-          {errors.email && (
-            <span>{errors.email}</span>
+          <Field className='input' name="cpf" type="text"/>
+          {errors.cpf && (
+           <p className='error'>{errors.cpf}</p>
           )}
         </div>
         <div>
           <label>Cidade</label>
-          <Field className='input' name="email" type="email"/>
-          {errors.email && (
-            <span>{errors.email}</span>
+          <Field className='input' name="cidade" type="text"/>
+          {errors.cidade && (
+           <p className='error'>{errors.cidade}</p>
           )}
         </div>
         <button type='submit'>Enviar</button>
--- src/schema.js
+++ src/schema.js
@@ -1,6 +1,9 @@
 import * as Yup from 'yup';
 
 export default Yup.object().shape({
-    name: Yup.string('Insira o Nome corretamente').min(2).required('Nome Obrigatório'),
+    name: Yup.string('Insira o Nome corretamente').min(2, 'Preencha mais de um caractere').required('Nome Obrigatório'),
     email: Yup.string().email('Insira o E-mail corretamente').required('E-mail Obrigatório'),
+    celular: Yup.string().required('Celular é Obrigatório'),
+    cpf: Yup.string().required('CPF é Obrigatório'),
+    cidade: Yup.string().required('Cidade é Obrigatória'),
 });
