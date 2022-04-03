c89177317ca0b3bf8c246ddd58594ea08baacebe
Caio Barbosa <caiobarbosasoriano@gmail.com>
2022-04-03 19:08:31 -0300

    adding RegExp

--- src/schema.js
+++ src/schema.js
@@ -1,9 +1,12 @@
 import * as Yup from 'yup';
 
+const celular = "^\([1-9]{2}\) [9]{0,1}[6-9]{1}[0-9]{3}\-[0-9]{4}$";
+const cpf = "([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})";
+
 export default Yup.object().shape({
     name: Yup.string('Insira o Nome corretamente').min(2, 'Preencha mais de um caractere').required('Nome Obrigatório'),
     email: Yup.string().email('Insira o E-mail corretamente').required('E-mail Obrigatório'),
-    celular: Yup.string().required('Celular é Obrigatório'),
-    cpf: Yup.string().required('CPF é Obrigatório'),
+    celular: Yup.string().matches(celular, 'Celular não é valido').required('Celular é obrigatório'),
+    cpf: Yup.string().matches(cpf, 'CPF é não é valido').required('CPF é obrigatório'),
     cidade: Yup.string().required('Cidade é Obrigatória'),
 });
